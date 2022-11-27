9b2e2a318c9a031bb279c8d37cf3d2cfa4760136
Muhammad Salman <msalmantariq786@gmail.com>
2022-11-27 12:03:36 +0500

    alert

--- Fashion Blog/Javascript/script.js
+++ Fashion Blog/Javascript/script.js
@@ -1,6 +1,12 @@
+// Name User 
 window.onload = function() {
     let message = ('Good Day !');
     let name = prompt ('Please Enter Your Name to visit the Website');
 
     document.getElementById('nameUser').innerHTML = message + " " + name; 
 }; 
+// Subscribe Button
+document.getElementById('btnSubscribe').onclick = function() { 
+    alert ("Please enter your Email Address to Subscribe");
+
+};
